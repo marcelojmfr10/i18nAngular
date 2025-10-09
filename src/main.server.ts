2a61e96e4e5a9a25dbc7426515d8257402f2074d
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-10-08 20:20:28 -0600

    Instalación tailwind

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
+import { provideServerRendering } from '@angular/ssr';
 
-const bootstrap = () => bootstrapApplication(App, config);
+// const bootstrap = () => bootstrapApplication(App, config);
 
-export default bootstrap;
+// export default bootstrap;
+
+export default function (context: BootstrapContext) {
+  return bootstrapApplication(App, {
+    ...config,
+    providers: [
+      provideServerRendering(),
+      ...(config.providers || []),
+    ]
+  }, context);
+}
